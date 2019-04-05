fa9b9925a1fcf1d175c575fde1b8a7abc78cb9ed
PAPAJ <mikolajkasperek@o2.pl>
2019-04-05 20:55:43 +0200

    bottom nav plus some fragments try

---
+++ Lab4/app/src/main/java/com/example/lab4/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.lab4;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation_menu);
+        configureBottomNavigationListener();
+
+        viewPager = (ViewPager) findViewById(R.id.main_fragment_container);
+
+    } // on create
+
+
+    private void configureViewPager() {
+        MyPagerAdapter myPagerAdapter = new MyPagerAdapter(getSupportFragmentManager());
+        myPagerAdapter.addFragment(new OptionsFragment(), "Menu");
+    }// configure view pager
+
+    private void configureBottomNavigationListener() {
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_menu_item_home:
+                        // odpal fragmenty menu
+                        return true;
+                    case R.id.nav_menu_item_dogs :
+                        // odpal fragment o psach
+                        return true;
+                    case R.id.nav_menu_item_cats :
+                        // odpal fragment o kotach
+                        return true;
+                }//switch
+                return false;
+            } // on nav item selected
+        });
+    } // bottom navigation listener configuration
+}// class
---
+++ Lab4/app/src/main/java/com/example/lab4/OptionsFragment.java
@@ -0,0 +1,19 @@
+package com.example.lab4;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OptionsFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.options_fragment, container, false);
+        return view;
+    }// on create view
+} // class
